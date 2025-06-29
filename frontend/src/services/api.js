7af320d311e46c2bb4b0eb0d8b1fda9116a7917d
JUBAYER AHMMED <jubayerahmmed105@gmail.com>
2025-06-29 16:26:41 +0600

    changed the url to the deployed one

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://time-saver.onrender.com",
   withCredentials: true, // ⬅️ required for session-based auth
 });
 
